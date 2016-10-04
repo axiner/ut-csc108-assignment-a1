8feb487350faffb2a3c6647174bfbd6e90400ff7
axiner <axiner.w@gmail.com>
2016-10-04 09:50:55 -0400

    Functions is_special_case and funtion get_final_mark written and tested, function get_both_marks started

--- admission_functions.py
+++ admission_functions.py
@@ -3,7 +3,8 @@ SPECIAL_CASE_SCHOOL_2 = 'Father Mercredi High School'
 SPECIAL_CASE_YEAR = '2016'
 
 # Add other constants here
-
+NO_EXAM = 'NE'
+DESIRED_COURSE = input("Enter the course code of the desired course: ")
 
 def is_special_case(record):
     """ (str) -> bool
@@ -19,6 +20,26 @@ def is_special_case(record):
     """
 
     # Complete the body of the function here
+    return SPECIAL_CASE_SCHOOL_1 in record and SPECIAL_CASE_YEAR in record
+    return SPECIAL_CASE_SCHOOL_2 in record and SPECIAL_CASE_YEAR in record
 
 
 # Complete the rest of the functions here
+
+def get_final_mark(record,coursework_mark,exam_mark):
+    
+    if NO_EXAM in exam_mark and is_special_case:
+        return float(coursework_mark)
+    elif NO_EXAM in exam_mark:
+        return (int(coursework_mark) + 0)/2
+    else:
+        return (int(coursework_mark)+int(exam_mark))/2
+    
+
+def get_both_marks(course_record,course_code):
+    
+    if DESIRED_COURSE == course_code:
+        return int
+    
+    
+
